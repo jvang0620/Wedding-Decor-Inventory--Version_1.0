774d169d424ef08d79f7e126040095c440772232
Jai_Vang <jaivang0387@gmail.com>
2024-02-20 08:21:09 -0500

    first commit

---
+++ App.java
@@ -0,0 +1,11 @@
+
+//import classes
+import src.weddingInventory.runApp;
+
+public class App {
+
+    public static void main(String[] args) {
+        runApp.displayHomePage();
+    }
+
+}
---
+++ src/weddingInventory/runApp.java
@@ -0,0 +1,15 @@
+package src.weddingInventory;
+
+public class runApp {
+
+    public static void displayHomePage() {
+
+        System.out.println("");
+        System.out.println("Wedding Inventory App \n");
+        System.out.println("1. View Inventory \n");
+        System.out.println("2. Create Inventory \n");
+        System.out.println("3. Generate Report \n");
+        System.out.println("4. Exit App \n");
+        System.out.println("**Please select a number (1 - 4)**");
+    }
+}
